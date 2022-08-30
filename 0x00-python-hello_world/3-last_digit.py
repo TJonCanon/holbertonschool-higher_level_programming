80ec76435aaf8e050753a3b67b69a14b7372ff92
TJonCanon <4923@holbertonstudents.com>
2022-08-30 14:59:38 -0500

    Task 3 0x00

---
+++ 0x00-python-hello_world/3-last_digit.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+print("Last digit of {}".format(number), end="")
+last_digit = int(repr(number)[-1])
+print(" is {}".format(last_digit), end="")
+if last_digit > 5:
+    print(" and is greater than 5")
+if last_digit < 6 and last_digit != 0:
+    print(" and is less than 6 and not 0")
+if last_digit == 0:
+    print(" and is 0")
