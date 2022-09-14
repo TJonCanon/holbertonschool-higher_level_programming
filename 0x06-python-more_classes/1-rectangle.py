05b5d78a101f7fb7f7e0ae9a3bf6a95dd6cadeb2
TJonCanon <taylor.joyner93@me.com>
2022-09-13 19:43:45 -0500

    idk

--- 0x06-python-more_classes/1-rectangle.py
+++ 0x06-python-more_classes/1-rectangle.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
-""" Rectangle time """
+""" rectangle time """
 
 
 class Rectangle:
-    """ class to make a rectange """
+    """ class to make a rectangle """
     def __init__(self, width=0, height=0):
         if type(width) is not int:
             raise TypeError("width must be an integer")
         if width < 0:
-            raise ValueError("width must >= 0")
-
+            raise ValueError("width must be >= 0")
         if type(height) is not int:
             raise TypeError("height must be an integer")
         if height < 0:
             raise ValueError("height must be >= 0")
-
         self.__width = width
         self.__height = height
 
@@ -28,7 +26,6 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-
         self.__width = value
 
     @property
@@ -41,13 +38,12 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-
         self.__height = value
 
     def area(self):
         return self.__height * self.__width
 
     def perimeter(self):
-        if 0 in (self.__height, self.__width)
+        if 0 in (self.__height, self.__width):
             return 0
         return (self.__width * 2) + (self.__height * 2)
