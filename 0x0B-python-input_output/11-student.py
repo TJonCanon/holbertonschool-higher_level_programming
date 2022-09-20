d90efe4a705a6ebb0106a1467dc064884f4a2fcb
TJonCanon <taylor.joyner93@me.com>
2022-09-19 20:05:15 -0500

    task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Student class """
+
+
+class Student:
+    """ Student class """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ dictionary """
+        if attrs is None:
+            return vars(self)
+        new_dict = {}
+        for i in attrs:
+            try:
+                new_dict[i] = vars(self)[i]
+            except Exception:
+                pass
+        return new_dict
+
+    def reload_from_json(self, json):
+        """ replaces attributes """
+        self.__dict__.update(json)
