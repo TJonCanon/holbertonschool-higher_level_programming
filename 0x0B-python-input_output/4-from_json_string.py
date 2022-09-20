18d285e0492acc4a3cd919ce55712a9591689e32
TJonCanon <taylor.joyner93@me.com>
2022-09-19 19:52:29 -0500

    task 4

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" object represented by a JSON string: """
+
+import json
+
+
+def from_json_string(my_str):
+    """ returns object represented by a JSON string: """
+    return json.loads(my_str)
