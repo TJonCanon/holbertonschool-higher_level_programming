20990c83d0cf803c5ed323a5d40c9ce2c7935756
TJonCanon <taylor.joyner93@me.com>
2022-09-23 16:11:58 -0500

    task 11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,3 +10,12 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """ place holder """
         super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """ defines size of square """
+        return self.width
+
+    @size.setter
+    def size(self, size):
+        self.width = size
