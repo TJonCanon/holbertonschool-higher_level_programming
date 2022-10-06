1aa2b5311a6d3de20f28852b45c29d633c92de1d
TJonCanon <taylor.joyner93@me.com>
2022-10-06 13:51:32 -0500

    forgot comma

--- 0x00-SQL_introduction/9-full_creation.sql
+++ 0x00-SQL_introduction/9-full_creation.sql
@@ -1,8 +1,8 @@
 -- create table second_table
 CREATE TABLE IF NOT EXISTS second_table (
     id INT,
-    name VARCHAR(256)
-    score INT);
+    name VARCHAR(256),
+    score INT );
 INSERT INTO second_table (id, name, score)
     VALUES (1, "John", 10),
     (2, "Alex", 3),
