469f6bdeb87c8b48d97e28288682dc408a11bc06
TJonCanon <taylor.joyner93@me.com>
2023-02-28 13:00:53 -0600

    task 3 pep8

--- 0x0B-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0B-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -2,8 +2,11 @@
 """Module contains a script that will list states in a MySQL database"""
 import MySQLdb
 from sys import argv
+
+
 def my_safe_filter_states():
-    """Takes in an argument and displays all values in the states table wher the argument is matchd"""
+    """Takes in an argument and displays all values in
+    the states table wher the argument is matchd"""
 
     db = MySQLdb.connect(host='localhost',
                          port=3306,
@@ -15,9 +18,8 @@ def my_safe_filter_states():
     cursor = db.cursor()
     name = argv[4]
     cursor.execute("SELECT * FROM states "
-                "WHERE BINARY name = %s "
-                "ORDER BY id", (name,)
-                )
+                   "WHERE BINARY name = %s "
+                   "ORDER BY id", (name,))
     rows = cursor.fetchall()
     for row in rows:
         print(row)
