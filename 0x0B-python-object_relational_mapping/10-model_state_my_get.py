4d8b9ac4d87893b3ed219bfddce3365bf1ac22c3
TJonCanon <taylor.joyner93@me.com>
2023-02-28 13:50:51 -0600

    task 10

--- 0x0B-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0B-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-""" a script that prints STATE
-    object with the NAME passed as argument from the database """
+"""lists all State objs that have the given name in db"""
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -10,7 +9,7 @@ from sys import argv
 if __name__ == '__main__':
     Session = sessionmaker(
         bind=create_engine(
-            'mysql+mysqldb://{}:{}@localhost:3306/'.format(
+            'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
                 argv[1], argv[2], argv[3]
             )
         )
