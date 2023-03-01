a38fd9dc5bb08ca9e4ade700039e6eee2875fcac
TJonCanon <taylor.joyner93@me.com>
2023-02-28 23:14:27 -0600

    task 5

--- 0x0B-python-object_relational_mapping/5-filter_cities.py
+++ 0x0B-python-object_relational_mapping/5-filter_cities.py
@@ -1,14 +1,38 @@
 #!/usr/bin/python3
+""" a script that will list states in a MySQL database """
 import MySQLdb
 from sys import argv
-if __name__ == '__main__':
-    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+
+
+def filter_cities():
+    """ takes in an argument and displays all values in
+    the states table where name matches the argument """
+
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3]
+                         )
+
     cursor = db.cursor()
-    cursor.execute("SELECT cities.name FROM cities JOIN states \
-                ON cities.state_id = states.id WHERE states.name=%s \
-                ORDER BY cities.id ASC", (state_name,))
+    name = argv[4]
+    cursor.execute("SELECT cities.name FROM cities\
+                 LEFT JOIN states\
+                 ON BINARY cities.state_id = states.id\
+                 WHERE states.id = (SELECT id\
+                                    FROM states\
+                                    WHERE name = %s)\
+                 ORDER BY cities.id", (name,))
     rows = cursor.fetchall()
-    if rows is not None:
-        print(", ".join(city[0] for city in rows))
+    output = ''
+    for row in rows:
+        output += row[0] + ', '
+    print(output[:-2])
+
     cursor.close()
     db.close()
+
+
+if __name__ == "__main__":
+    filter_cities()
