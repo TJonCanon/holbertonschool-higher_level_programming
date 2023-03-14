f45b630e6a01eda60c5d3edd8c68b39e3d1cf557
TJonCanon <taylor.joyner93@me.com>
2023-03-14 14:41:17 -0500

    task 0 trying something new

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-""" pulls https://intranet.hbtn.io/status """
-from urllib import request
+""" Task 0 """
 
 if __name__ == "__main__":
-    """ import guard """
+    from urllib import request
     with request.urlopen("https://intranet.hbtn.io/status") as resp:
         resp = resp.read()
-        print("Body response:")
-        print("\t - type: {}".format(type(resp)))
-        print("\t- content: {}".format(resp))
-        print("\t- utf8 content: {}".format(resp.decode('utf-8')))
+        print("""Body response:
+\t- type: {}
+\t- content: {}
+\t- utf8 content: {}""".format(type(resp), resp, resp.decode("utf8")))
