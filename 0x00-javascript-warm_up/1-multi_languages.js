9edee06b3e0f3a86c9c0864821da9a0cf81f00ca
TJonCanon <taylor.joyner93@me.com>
2023-04-06 11:45:41 -0500

    semistandard 1

--- 0x00-javascript-warm_up/1-multi_languages.js
+++ 0x00-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const fun_C = 'C is fun'
-const cool_P = 'Python is cool'
+const funC = 'C is fun';
+const coolP = 'Python is cool';
 const myVar = 'JavaScript is amazing';
-console.log(fun_C);
-console.log(cool_P);
+console.log(funC);
+console.log(coolP);
 console.log(myVar);
