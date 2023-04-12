4a9133801560ecdc98df545b03eb9e6debf303ea
TJonCanon <taylor.joyner93@me.com>
2023-04-11 22:52:36 -0500

    task 11

---
+++ 0x00-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+if (process.argv.length < 4) {
+  console.log(0);
+} else {
+  const array = process.argv.slice(2);
+  array.sort(function (a, b) {
+    return a - b;
+  });
+  array.reverse();
+  console.log(array[1]);
+}
